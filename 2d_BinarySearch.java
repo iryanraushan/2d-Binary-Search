680191664fb71bdfc47cefda044e67b0c36927aa
Raushan Ryan <83304272+iryanraushan@users.noreply.github.com>
2024-03-23 15:54:22 +0530

    Update 2d_BinarySearch_Linear.java

--- 2d_BinarySearch.java
+++ 2d_BinarySearch.java
@@ -1,1 +1,16 @@
 
+public static boolean search(int[][] mat, int target){
+        if (n == 0)
+            return -1;
+ 
+        // traverse through the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                // if the element is found
+                if (mat[i][j] == target) {
+                    return true;
+                }
+        }
+        // is the element is not found
+        return false;
+}
